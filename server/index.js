3d5b99727bef51cc24030167b4b360b1a05f4f5f
Jordan831 <vicky.electron831@gmail.com>
2021-03-11 03:59:14 +0530

    yml

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express= require('express');
 const app = express();
 
+
+app.get("/",(req,res,next)=>{
+    try
+    {
+    const p=0;
+    p=1;
+    res.send("finally i solved problem and working on private network");
+    }
+    catch(err)
+    {
+        next(err);
+    }
+});
 app.get("/api",(req,res)=>{
+    
     res.send("finally i solved problem and working on private network");
 });
 
-
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
 const port = process.env.PORT || 8000;
 app.listen(port);
