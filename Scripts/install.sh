fcba07229f9bf9854c71424ede189f617c8df0ff
Jordan831 <vicky.electron831@gmail.com>
2021-03-11 16:55:31 +0530

    yml

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo rm -R   /var/www/public_html/server/
-
-sudo rm -R   /var/www/public_html/client/
+if [ -z "$(ls -A /var/www/public_html/)" ]; then
+   echo "Empty"
+else
+sudo rm -R  /var/www/public_html/server
+sudo rm -R  /var/www/public_html/client
+fi
